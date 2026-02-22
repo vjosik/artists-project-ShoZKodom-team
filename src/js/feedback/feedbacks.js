19e6af8f9712570efd8cac7f46ae8f41636e7f82
Artem Bagno <bgn.rtm@gmail.com>
2026-02-22 16:42:42 +0200

    add function by Zahlyda and fix my code

--- src/js/feedback/feedbacks.js
+++ src/js/feedback/feedbacks.js
@@ -40,17 +40,81 @@ function addRatings(rat = 0) {
   }
   return renderRating;
 }
+/*Блок АНАСТАСИИ*/
 
+import Raty from 'raty-js';
+
+const STAR_ON_SVG = `
+<svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9.07088 0.612343C9.41462 -0.204115 10.5854 -0.204114 10.9291 0.612346L12.9579 5.43123C13.1029 5.77543 13.4306 6.01061 13.8067 6.0404L19.0727 6.45748C19.9649 6.52814 20.3267 7.62813 19.6469 8.2034L15.6348 11.5987C15.3482 11.8412 15.223 12.2218 15.3106 12.5843L16.5363 17.661C16.744 18.5211 15.7969 19.201 15.033 18.7401L10.5245 16.0196C10.2025 15.8252 9.7975 15.8252 9.47548 16.0196L4.96699 18.7401C4.20311 19.201 3.25596 18.5211 3.46363 17.661L4.68942 12.5843C4.77698 12.2218 4.65182 11.8412 4.36526 11.5987L0.353062 8.2034C-0.326718 7.62813 0.0350679 6.52814 0.927291 6.45748L6.19336 6.0404C6.5695 6.01061 6.89716 5.77543 7.04207 5.43123L9.07088 0.612343Z" fill="#764191"/>
+</svg>
+`;
+
+const STAR_OFF_SVG = `
+<svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9.07088 0.612343C9.41462 -0.204115 10.5854 -0.204114 10.9291 0.612346L12.9579 5.43123C13.1029 5.77543 13.4306 6.01061 13.8067 6.0404L19.0727 6.45748C19.9649 6.52814 20.3267 7.62813 19.6469 8.2034L15.6348 11.5987C15.3482 11.8412 15.223 12.2218 15.3106 12.5843L16.5363 17.661C16.744 18.5211 15.7969 19.201 15.033 18.7401L10.5245 16.0196C10.2025 15.8252 9.7975 15.8252 9.47548 16.0196L4.96699 18.7401C4.20311 19.201 3.25596 18.5211 3.46363 17.661L4.68942 12.5843C4.77698 12.2218 4.65182 11.8412 4.36526 11.5987L0.353062 8.2034C-0.326718 7.62813 0.0350679 6.52814 0.927291 6.45748L6.19336 6.0404C6.5695 6.01061 6.89716 5.77543 7.04207 5.43123L9.07088 0.612343Z" fill="white"/>
+</svg>
+`;
+
+const STAR_HALF_SVG = `
+<svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <clipPath id="halfClip">
+      <rect x="0" y="0" width="10" height="19"></rect>
+    </clipPath>
+  </defs>
+  <path d="M9.07088 0.612343C9.41462 -0.204115 10.5854 -0.204114 10.9291 0.612346L12.9579 5.43123C13.1029 5.77543 13.4306 6.01061 13.8067 6.0404L19.0727 6.45748C19.9649 6.52814 20.3267 7.62813 19.6469 8.2034L15.6348 11.5987C15.3482 11.8412 15.223 12.2218 15.3106 12.5843L16.5363 17.661C16.744 18.5211 15.7969 19.201 15.033 18.7401L10.5245 16.0196C10.2025 15.8252 9.7975 15.8252 9.47548 16.0196L4.96699 18.7401C4.20311 19.201 3.25596 18.5211 3.46363 17.661L4.68942 12.5843C4.77698 12.2218 4.65182 11.8412 4.36526 11.5987L0.353062 8.2034C-0.326718 7.62813 0.0350679 6.52814 0.927291 6.45748L6.19336 6.0404C6.5695 6.01061 6.89716 5.77543 7.04207 5.43123L9.07088 0.612343Z" fill="white"/>
+  <path clip-path="url(#halfClip)" d="M9.07088 0.612343C9.41462 -0.204115 10.5854 -0.204114 10.9291 0.612346L12.9579 5.43123C13.1029 5.77543 13.4306 6.01061 13.8067 6.0404L19.0727 6.45748C19.9649 6.52814 20.3267 7.62813 19.6469 8.2034L15.6348 11.5987C15.3482 11.8412 15.223 12.2218 15.3106 12.5843L16.5363 17.661C16.744 18.5211 15.7969 19.201 15.033 18.7401L10.5245 16.0196C10.2025 15.8252 9.7975 15.8252 9.47548 16.0196L4.96699 18.7401C4.20311 19.201 3.25596 18.5211 3.46363 17.661L4.68942 12.5843C4.77698 12.2218 4.65182 11.8412 4.36526 11.5987L0.353062 8.2034C-0.326718 7.62813 0.0350679 6.52814 0.927291 6.45748L6.19336 6.0404C6.5695 6.01061 6.89716 5.77543 7.04207 5.43123L9.07088 0.612343Z" fill="#764191"/>
+</svg>
+`;
+
+const toDataUri = svg =>
+  `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`;
+
+const STAR_ON = toDataUri(STAR_ON_SVG);
+const STAR_OFF = toDataUri(STAR_OFF_SVG);
+const STAR_HALF = toDataUri(STAR_HALF_SVG);
+
+export function initFeedbackRatings() {
+  const ratingEls = document.querySelectorAll('.feedback-stars-rating');
+
+  ratingEls.forEach(el => {
+    if (el.dataset.ratyInitialized === 'true') return;
+
+    let score = Number(el.dataset.score);
+    if (Number.isNaN(score)) score = 0;
+    score = Math.max(0, Math.min(5, score));
+
+    const raty = new Raty(el, {
+      readOnly: true,
+      score,
+      number: 5,
+      halfShow: true,
+
+      starType: 'img',
+      starOn: STAR_ON,
+      starOff: STAR_OFF,
+      starHalf: STAR_HALF,
+
+      space: false,
+    });
+
+    raty.init?.();
+
+    el.dataset.ratyInitialized = 'true';
+  });
+}
+
+/**/
 const feedbackListEl = document.querySelector('.feedback-swiper-wrapper');
 
 export function createFeedbackList({ data }) {
   const markup = data
     .map(
-      ({ name, rating, descr } = item) => `
+      ({ name, rating, descr }) => `
         <li class="feedback-swiper-slide swiper-slide">
           <div class="feedback-card">
             <div class="feedback-stars-rating" data-score="${rating}">
-            ${addRatings(rating)}
             </div>
             <p class="feedback-text">
               "${descr}"
@@ -61,4 +125,5 @@ export function createFeedbackList({ data }) {
     )
     .join('');
   feedbackListEl.innerHTML = markup;
+  initFeedbackRatings();
 }
