c4e52779d35dbf27befc954ab321dd23603f969b
Artem Bagno <bgn.rtm@gmail.com>
2026-02-25 23:46:08 +0200

    finaly fix mobile menu

--- src/js/close-open.js/mobile-menu.js
+++ src/js/close-open.js/mobile-menu.js
@@ -1,26 +1,24 @@
 import { lockBodyScroll, unlockBodyScroll } from '../utills/scrolling';
 
-const openBtn = document.querySelector('.mobil-menu-btn')
-const closeBtn = document.querySelector('.menu-close-btn');   
-const menuContent = document.querySelector('.mobile-menu'); 
+const openBtn = document.querySelector('.mobil-menu-btn');
+const closeBtn = document.querySelector('.mm-close-btn');
+const menuContent = document.querySelector('.mobile-menu');
 const links = document.querySelectorAll('.menu-item');
 
 openBtn.addEventListener('click', () => {
   menuContent.classList.add('is-open');
-  document.body.classList.add("no-scroll");
+  document.body.classList.add('no-scroll');
   lockBodyScroll();
 });
 
 closeBtn.addEventListener('click', () => {
-  menuContent.classList.remove('is-open'); 
-   unlockBodyScroll();
-  
+  menuContent.classList.remove('is-open');
+  unlockBodyScroll();
 });
 
 links.forEach(link => {
   link.addEventListener('click', () => {
-    menuContent.classList.remove('is-open'); 
-     unlockBodyScroll();
+    menuContent.classList.remove('is-open');
+    unlockBodyScroll();
   });
 });
-
